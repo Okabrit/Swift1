1555111619d8c5c3ae063d59e45e2f23ff244c24
Okabrit <40790212+Okabrit@users.noreply.github.com>
2018-07-06 11:43:12 +0300

    Add files via upload

---
+++ Snake2.swift
@@ -0,0 +1,36 @@
+let button = UIButton(x : 100, y: 100,height: 100,width: 100)
+ button.backgroundColor = .greenColor()
+ button.setTitle("w", forState: Normal)
+ button.addTarget(self, action: #selector(buttonAction), forControlEvents.TouchUpInside){
+ self.view.addSubViewiew(button)
+ }
+ func buttonAction(sender: UIButton!){
+ print(" ")
+ }
+ let button = UIButton(x : 150, y: 150,height: 100,width: 100)
+ button.backgroundColor = .greenColor()
+ button.setTitle("d", forState: Normal)
+ button.addTarget(self, action: #selector(buttonAction), forControlEvents.TouchUpInside){
+ self.view.addSubViewiew(button)
+ }
+ func buttonAction(sender: UIButton!){
+ print(" ")
+ }
+ let button = UIButton(x : 200, y: 200,height: 100,width: 100)
+ button.backgroundColor = .greenColor()
+ button.setTitle("a", forState: Normal)
+ button.addTarget(self, action: #selector(buttonAction), forControlEvents.TouchUpInside){
+ self.view.addSubViewiew(button)
+ }
+ func buttonAction(sender: UIButton!){
+ print(" ")
+ }
+ let button = UIButton(x : 250, y: 250,height: 100,width: 100)
+ button.backgroundColor = .greenColor()
+ button.setTitle("s", forState: Normal)
+ button.addTarget(self, action: #selector(buttonAction), forControlEvents.TouchUpInside){
+ self.view.addSubViewiew(button)
+ }
+ func buttonAction(sender: UIButton!){
+ print(" ")
+ }
